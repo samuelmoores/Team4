71152a874c76b7b578d5430db72ab1e3bcd7974c
Miguel Leyva <Miguel.Leyva01@student.csulb.edu>
2026-04-07 02:24:36 -0700

    Create cs file that prints Hello World

---
+++ HelloWorld.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Testing
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
